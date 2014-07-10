d4e223ef64b7637482e53daed35c02c0b6bb67b8
Nathaniel van Diepen <eeems@omnimaga.org>
2014-07-10 00:02:57 -0600

    See Description
    Require friendly.
    log.get() added
    rewrote listdb.js to be non-blocking.
    - order going into the log file could be different.
    |  - not a problem if you use a primary key
    - renamed functions
    | - listdb.getDB() to listdb.get()
    | - db.getAll() to db.all()
    | - db.getName() to db.name()
    \ - db.hasValue() to db.has()

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('./lib/main.js');
+module.exports = {
+	Server: require('./lib/server.js'),
+	Channel: require('./lib/channel.js'),
+	User: require('./lib/user.js'),
+	api: require('./lib/api.js'),
+	stdin: require('./lib/stdin.js'),
+	log: require('./lib/log.js'),
+	debug: require('./lib/debug.js'),
+	listdb: require('./lib/listdb.js'),
+	help: require('./lib/help.js')
+};
