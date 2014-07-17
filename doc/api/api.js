1581e8486fdc651e298514f6026a619961be6b05
Nathaniel van Diepen <eeems@omnimaga.org>
2014-07-17 00:05:53 -0600

    Finalize documentation. Small fixes

--- doc/api/api.js
+++ doc/api/api.js
@@ -2,24 +2,72 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "Channel",
+        "Listdb",
         "Script",
-        "User"
+        "User",
+        "api",
+        "debug",
+        "log",
+        "server",
+        "stdin",
+        "tools"
     ],
     "modules": [
+        "api",
         "channel",
+        "debug",
+        "listdb",
+        "log",
         "script",
+        "server",
+        "stdin",
+        "tools",
         "user"
     ],
     "allModules": [
+        {
+            "displayName": "api",
+            "name": "api",
+            "description": "API class, contains references to all important modules"
+        },
         {
             "displayName": "channel",
             "name": "channel",
             "description": "Channel class"
         },
         {
+            "displayName": "debug",
+            "name": "debug",
+            "description": "Debug functions"
+        },
+        {
+            "displayName": "listdb",
+            "name": "listdb",
+            "description": "listdb class"
+        },
+        {
+            "displayName": "log",
+            "name": "log",
+            "description": "Logging module to handle outputting logs with the correct format, and saving them to files"
+        },
+        {
             "displayName": "script",
             "name": "script",
             "description": "Provides a script object that allows a server to handle the loaded script"
+        },
+        {
+            "displayName": "server",
+            "name": "server",
+            "description": "IRC Server object"
+        },
+        {
+            "displayName": "stdin",
+            "name": "stdin",
+            "description": "stdin control"
+        },
+        {
+            "displayName": "tools",
+            "name": "tools"
         },
         {
             "displayName": "user",
