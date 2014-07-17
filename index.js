9c6236125aa3e64e9e2ff44aeecae0f453677bbd
Nathaniel van Diepen <eeems@omnimaga.org>
2014-07-16 22:27:41 -0600

    Clean up old references to help.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-module.exports = {
-	Server: require('./lib/server.js'),
-	Channel: require('./lib/channel.js'),
-	User: require('./lib/user.js'),
-	api: require('./lib/api.js'),
-	stdin: require('./lib/stdin.js'),
-	log: require('./lib/log.js'),
-	debug: require('./lib/debug.js'),
-	listdb: require('./lib/listdb.js'),
-	help: require('./lib/help.js')
-};
+module.exports = require('./lib/api.js');
