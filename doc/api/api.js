eda8643523e870be2070b418158f25b4ba05674e
Nathaniel van Diepen <eeems@omnimaga.org>
2014-07-16 21:32:19 -0600

    Initial work on documentation

---
+++ doc/api/api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Channel",
+        "Script",
+        "User"
+    ],
+    "modules": [
+        "channel",
+        "script",
+        "user"
+    ],
+    "allModules": [
+        {
+            "displayName": "channel",
+            "name": "channel",
+            "description": "Channel class"
+        },
+        {
+            "displayName": "script",
+            "name": "script",
+            "description": "Provides a script object that allows a server to handle the loaded script"
+        },
+        {
+            "displayName": "user",
+            "name": "user",
+            "description": "User object constructor"
+        }
+    ]
+} };
+});
