f329bde49ec65a6bf22e548d88fd9ab96abe813d
Nathaniel van Diepen <eeems@omnimaga.org>
2014-07-23 16:04:33 -0600

    See Description
    Convert scripts/manager.js
    Add built in help command.
    Handle server.quit() properly (delete socket from server after)

--- scripts/manager.js
+++ scripts/manager.js
@@ -1,20 +1,10 @@
-listen(rCommand('uptime'),function(match,data,replyTo,connection){
-	if(validUser(match[2],match[1])&&isOp(match[2])){
-		connection.reply(replyTo,"Uptime: "+process.uptime());
-	}
-});
-listen(rCommand('exit'),function(match,data,replyTo,connection){
-	if(validUser(match[2],match[1])&&isOp(match[2])){
-		exit();
-	}
-});
-listen(rCommand('quit'),function(match,data,replyTo,connection){
-	if(validUser(match[2],match[1])&&isOp(match[2])){
-		connection.quit();
-	}
-});
-regHelp('uptime','returns the current uptime of the bot');
-regHelp('exit',"disconnects from all connections and exits the bot's process");
-regHelp('quit','disconnect from the current server');
-regHelp('disable','disables a script');
-disp.alert('manager script finished');
+server.add('uptime',function(){
+	server.send('PRIVMSG '+this.channel.name+' :'+process.uptime());
+},'Displays the current uptime of the bot');
+server.add('exit',function(){
+	process.exit();
+},'Makes the bot exit');
+server.add('quit',function(){
+	server.quit();
+	log.log('quitting');
+},'Makes the bot quit from the current server');
