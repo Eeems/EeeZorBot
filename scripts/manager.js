3532916a9d8160da1f7aa3729c79b274c8df4450
Nathaniel van Diepen <eeems@omnimaga.org>
2014-07-25 13:48:08 -0600

    DERP, server.on() and server.add() are chainable, use it!

--- scripts/manager.js
+++ scripts/manager.js
@@ -1,10 +1,10 @@
 server.add('uptime',function(){
-	server.send('PRIVMSG '+this.channel.name+' :'+process.uptime());
-},'Displays the current uptime of the bot');
-server.add('exit',function(){
-	process.exit();
-},'Makes the bot exit');
-server.add('quit',function(){
-	server.quit();
-	log.log('quitting');
-},'Makes the bot quit from the current server');
+		server.send('PRIVMSG '+this.channel.name+' :'+process.uptime());
+	},'Displays the current uptime of the bot')
+	.add('exit',function(){
+		process.exit();
+	},'Makes the bot exit')
+	.add('quit',function(){
+		server.quit();
+		log.log('quitting');
+	},'Makes the bot quit from the current server');
