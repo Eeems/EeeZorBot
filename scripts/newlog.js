62105b6bf76b7e0a7f46fd098be90fece6589d6f
Nathaniel van Diepen <eeems@omnimaga.org>
2014-12-04 14:28:10 -0700

    db.js work

--- scripts/newlog.js
+++ scripts/newlog.js
@@ -1,36 +1,20 @@
 /*jshint multistr: true */
 var server;
-db.query("\
-	CREATE TABLE IF NOT EXISTS logs (\
-		id int AUTO_INCREMENT PRIMARY KEY,\
-		date datetime,\
-		t_id int,\
-		msg varchar(512)\
-	)\
- ",function(e){
-	if(e){
-		throw e;
-	}
-});
-db.query("\
-	CREATE TABLE IF NOT EXISTS types (\
-		id int AUTO_INCREMENT PRIMARY KEY,\
-		name varchar(10) UNIQUE KEY\
-	)\
- ",function(e){
-	if(e){
-		throw e;
-	}
-});
-db.hasIndex('logs','test',function(f){
-	if(!f){
-		db.query("\
-			CREATE INDEX i_logs_t_id\
-			ON logs (t_id)\
-		",function(e){
-			if(e){
-				throw e;
-			}
-		});
-	}
-});
+db.multiQuerySync([
+	"\
+		CREATE TABLE IF NOT EXISTS logs (\
+			id int AUTO_INCREMENT PRIMARY KEY,\
+			date datetime,\
+			t_id int,\
+			msg varchar(512)\
+		)\
+	",
+	"\
+		CREATE TABLE IF NOT EXISTS types (\
+			id int AUTO_INCREMENT PRIMARY KEY,\
+			name varchar(10) UNIQUE KEY\
+		)\
+	"
+]);
+db.createIndexSync('logs','i_logs_t_id','t_id');
+process.exit();
