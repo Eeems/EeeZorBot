2a71163dcecd1e113129c9b1ec2c5d7825fd32f5
Nathaniel van Diepen <eeems@omnimaga.org>
2015-07-04 00:58:55 -0600

    Basic stats

--- mods/log/scripts/http.js
+++ mods/log/scripts/http.js
@@ -18,6 +18,12 @@ var settings = (function(){
 		log: template(_dirname+'/../www/log.html'),
 		user: template(_dirname+'/../www/log/user.html'),
 		'404': template(_dirname+'/../www/404.html'),
+		stats: {
+			index: template(_dirname+'/../www/stats/index.html'),
+			server: template(_dirname+'/../www/stats/server.html'),
+			channel: template(_dirname+'/../www/stats/channel.html'),
+			user: template(_dirname+'/../www/stats/user.html')
+		},
 		types: {
 			action: template(_dirname+'/../www/log/types/action.html'),
 			datechange: template(_dirname+'/../www/log/types/datechange.html'),
@@ -105,213 +111,378 @@ var settings = (function(){
 						}));
 						res.end();
 					});
-				}else if(args.length == 1){
-					db.query("\
-						SELECT	id,\
-								name,\
-								s_id\
-						FROM channels\
-						WHERE s_id = ?\
-						AND name like '#%'\
-					",[args[0]],function(e,r){
-						if(e){
-							throw e;
-						}
-						var server = db.querySync("select name from servers where id = ?",[args[0]])[0];
-						if(server!==undefined){
-							res.write(templates.server.compile({
-								name: server.name,
-								channels: r
-							}));
-						}else{
-							res.statusCode = 404;
-							res.write(templates['404'].compile({
-								message: 'Server does not exist'
-							}));
-						}
-						res.end();
-					});
-				}else{
-					var ts = function(d){
-							return d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
-						},
-						d = new Date(+new Date),
-						pastDate,
-						nextDate,
-						a,
-						date,
-						controls;
-					if(args[2]===undefined){
-						res.writeHead(302,{
-							Location: req.url+'/'+ts(d)
-						});
-					}
-					args[2] = args[2]===undefined?ts(d):args[2];
-					a = args[2].split('-');
-					date = new Date(a[0],parseInt(a[1],10)-1,a[2]);
-					pastDate = new Date(date.getTime()-(24*60*60*1000));
-					nextDate = new Date(date.getTime()+(24*60*60*1000));
-					d= new Date(d.getFullYear(),d.getMonth(),d.getDate());
-					db.query("\
-						SELECT	m.id,\
-								u.name AS user,\
-								t.name AS type,\
-								m.text,\
-								DATE_FORMAT(m.date,'%H:%i:%s') as time,\
-								DATE_FORMAT(m.date,'%Y-%m-%dT%H:%i:%sZ') as datetime\
-						FROM messages m\
-						JOIN types t\
-							ON t.id = m.t_id\
-						JOIN users u\
-							ON u.id = m.u_id\
-						WHERE m.date >= STR_TO_DATE(?,'%Y-%m-%d')\
-						AND m.date <= DATE_ADD(STR_TO_DATE(?,'%Y-%m-%d'),INTERVAL 1 DAY)\
-						AND m.c_id = ?\
-						ORDER BY m.date ASC\
-					",[args[2],args[2],args[1]],function(e,r){
-						if(e){
-							throw e;
-						}
-						var server = db.querySync("select name from servers where id = ?",[args[0]])[0],
-							channel = db.querySync("select name from channels where id = ? and name like '#%'",[args[1]])[0];
-						if(server!==undefined&&channel!==undefined){
-								var data = {
-										s_id: args[0],
-										server: server.name,
-										c_id: args[1],
-										channel: channel.name,
-										date: args[2],
-										pastDate: ts(pastDate),
-										todayDate: ts(d),
-										nextDate: ts(nextDate),
-										messages: []
-									},
-									m,t,
-									htmlent = function(text){
-										return String(text).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-									},
-									parse = function(m){
-										var style="",
-											c,
-											bg,
-											t=m[0];
-										switch(t){
-											case "\x0f":
-												t = "\x03";
-											break;
-											case "\x1f":
-												t = "\x03";
-												style="text-decoration:underline;";
-											break;
-											case "\x02":
-												t="\x03";
-												style="font-weight:bold;";
-											break;
-											case "\x03":
-												c=m[1];
-												if(/\d/.test(m[2])){
-													c+=m[2];
-													if(m[3] == ','){
-														bg = m[4];
-														if(/\d/.test(m[5])){
-															bg+=m[5];
-														}
-													}
-												}else if(m[2] == ','){
-													bg = m[3];
-													if(/\d/.test(m[4])){
-														bg+=m[4];
-													}
+				}else if(args.length > 0){
+					switch(args[0]){
+						case 'stats':
+							if(args.length == 1){
+								db.query("\
+									SELECT	id,\
+											name\
+									FROM servers\
+								",function(e,servers){
+									if(e){
+										throw e;
+									}
+									res.write(templates.stats.index.compile({
+										servers: servers
+									}));
+									res.end();
+								});
+							}else{
+								if(args.length<4 && args.length>2){
+									switch(args[1]){
+										case 'user':
+											db.query("\
+												select	name,\
+														id\
+												from users\
+												where id = ?\
+											",[args[2]],function(e,user){
+												if(e){
+													throw e;
 												}
-											break;
-										}
-										return '</span>'+chunk(c,bg,style);
-								},
-								colourNick = function(nick){
-									nick = htmlent(nick);
-									var hash = (function(){
-											var h = 0,
-												i;
-											for(i=0;i<nick.length;i++){
-												h = nick.charCodeAt(i)+(h<<6)+(h<<16)-h;
-											}
-											return h;
-										})(),
-										deg = hash%360,
-										hue = deg<0?360+deg:deg,
-										light = hue>=30&&hue<=210?30:50,
-										saturation = 20+Math.abs(hash)%80;
-									return templates.user.compile({
-										nick: nick,
-										hue: hue,
-										saturation: saturation,
-										light: light
-									});
-								},
-								getColour = function(num,def){
-									var c = [
-										'white',
-										'black',
-										'blue',
-										'green',
-										'red',
-										'brown',
-										'purple',
-										'orange',
-										'yellow',
-										'lime',
-										'teal',
-										'aqua',
-										'royalblue',
-										'fuchsia',
-										'grey',
-										'silver'
-									][parseInt(num,0)];
-									return c===undefined?def:c;
-								},
-								chunk = function(c,bg,style){
-									style = style===undefined?'':style;
-									return "<span style='color:"+getColour(c,'black')+";background-color:"+getColour(bg,'transparent')+";"+style+"'>";
-								},
-								links = function(href){
-									return isdomain(href)?'<a href="'+toUrl(href)+'">'+href+'</a>':href;
-								},
-								ds = {},
-								id,
-								i;
-							for(i in r){
-								m = r[i];
-								t = htmlent(m.text)
-									.replace(/\b((?:\w*:?\/\/)?\w+\.\w\w+\/?[A-Za-z0-9_.-~\-]*#?[A-Za-z0-9_.\-~\-]*)\b/g,links)
-									.replace(/[\x02\x1f\x16\x0f]|\x03(\d{0,2}(?:,\d{0,2})?)/g,parse)
-									.trim();
-								id = m.time;
-								if(ds[id]!==undefined){
-									id = id+'-'+m.id;
+												user = user[0];
+												db.query("\
+													select	c.s_id,\
+															s.name as server,\
+															c.id,\
+															c.name,\
+															count(m.id) as \"lines\"\
+													from channels c\
+													join servers s\
+														on s.id = c.s_id\
+													left outer join messages m\
+														on m.c_id = c.id\
+													where m.u_id = ?\
+														and left(c.name,1) = '#'\
+													group by c.s_id,c.id\
+												",[user.id],function(e,channels){
+													if(e){
+														throw e;
+													}
+													user.channels = channels;
+													res.write(templates.stats.user.compile(user));
+													res.end();
+												});
+											});
+										break;
+										case 'channel':
+											db.query("\
+												select	c.name,\
+														c.id,\
+														c.s_id,\
+														s.name as server\
+												from channels c\
+												join servers s\
+													on s.id = c.s_id\
+												where c.id = ?\
+											",[args[2]],function(e,channel){
+												if(e){
+													throw e;
+												}
+												channel = channel[0];
+												db.query("\
+													select	u.id,\
+															u.name,\
+															count(m.id) as \"lines\"\
+													from channels c\
+													left outer join messages m\
+														on m.c_id = c.id\
+													join users u\
+														on u.id = m.u_id\
+													where c.id = ?\
+													group by u.id\
+												",[channel.id],function(e,users){
+													if(e){
+														throw e;
+													}
+													channel.users = users;
+													res.write(templates.stats.channel.compile(channel));
+													res.end();
+												});
+											});
+										break;
+										case 'server':
+											db.query("\
+												select	name,\
+														id\
+												from servers\
+												where id = ?\
+											",[args[2]],function(e,server){
+												if(e){
+													throw e;
+												}
+												server = server[0];
+												db.query("\
+													select	c.id,\
+															c.name,\
+															count(m.id) as \"lines\",\
+															count(distinct u.id) as users\
+													from channels c\
+													left outer join messages m\
+														on m.c_id = c.id\
+													join users u\
+														on u.id = m.u_id\
+													where c.s_id = ?\
+														and left(c.name,1) = '#'\
+													group by c.id\
+												",[server.id],function(e,channels){
+													if(e){
+														throw e;
+													}
+													server.channels = channels;
+													db.query("\
+														select	u.id,\
+																u.name,\
+																count(m.id) as \"lines\",\
+																count(distinct c.id) as channels\
+														from users u\
+														left outer join messages m\
+															on m.u_id = u.id\
+														join channels c\
+															on c.id = m.c_id\
+														where c.s_id = ?\
+															and left(c.name,1) = '#'\
+														group by u.id\
+													",[server.id],function(e,users){
+														if(e){
+															throw e;
+														}
+														server.users = users;
+														res.write(templates.stats.server.compile(server));
+														res.end();
+													});
+												});
+											});
+										break;
+										default:
+											res.statusCode = 404;
+											res.write(templates['404'].compile({
+												message: 'Not Implemented'
+											}));
+											res.end();
+									}
+								}else{
+									res.statusCode = 404;
+									res.write(templates['404'].compile({
+										message: 'Invalid arguments'
+									}));
+									res.end();
 								}
-								ds[id] = true;
-								data.messages.push({
-									id: id,
-									type: m.type,
-									datetime: m.datetime,
-									time: m.time,
-									body: chunk()+(templates.types[m.type]?templates.types[m.type]:templates.types.message).compile({
-										user: colourNick(m.user),
-										text: t,
-										channel: channel.name,
-										server: server.name,
-									})+'</span>'
+							}
+						break;
+						default:
+							if(args.length == 1){
+								db.query("\
+									SELECT	id,\
+											name,\
+											s_id\
+									FROM channels\
+									WHERE s_id = ?\
+									AND name like '#%'\
+								",[args[0]],function(e,r){
+									if(e){
+										throw e;
+									}
+									var server = db.querySync("select name from servers where id = ?",[args[0]])[0];
+									if(server!==undefined){
+										res.write(templates.server.compile({
+											name: server.name,
+											channels: r
+										}));
+									}else{
+										res.statusCode = 404;
+										res.write(templates['404'].compile({
+											message: 'Server does not exist'
+										}));
+									}
+									res.end();
+								});
+							}else{
+								var ts = function(d){
+										return d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+									},
+									d = new Date(+new Date),
+									pastDate,
+									nextDate,
+									a,
+									date,
+									controls;
+								if(args[2]===undefined){
+									res.writeHead(302,{
+										Location: req.url+'/'+ts(d)
+									});
+								}
+								args[2] = args[2]===undefined?ts(d):args[2];
+								a = args[2].split('-');
+								date = new Date(a[0],parseInt(a[1],10)-1,a[2]);
+								pastDate = new Date(date.getTime()-(24*60*60*1000));
+								nextDate = new Date(date.getTime()+(24*60*60*1000));
+								d= new Date(d.getFullYear(),d.getMonth(),d.getDate());
+								db.query("\
+									SELECT	m.id,\
+											m.u_id,\
+											u.name AS user,\
+											t.name AS type,\
+											m.text,\
+											DATE_FORMAT(m.date,'%H:%i:%s') as time,\
+											DATE_FORMAT(m.date,'%Y-%m-%dT%H:%i:%sZ') as datetime\
+									FROM messages m\
+									JOIN types t\
+										ON t.id = m.t_id\
+									JOIN users u\
+										ON u.id = m.u_id\
+									WHERE m.date >= STR_TO_DATE(?,'%Y-%m-%d')\
+									AND m.date <= DATE_ADD(STR_TO_DATE(?,'%Y-%m-%d'),INTERVAL 1 DAY)\
+									AND m.c_id = ?\
+									ORDER BY m.date ASC\
+								",[args[2],args[2],args[1]],function(e,r){
+									if(e){
+										throw e;
+									}
+									var server = db.querySync("select name from servers where id = ?",[args[0]])[0],
+										channel = db.querySync("select name from channels where id = ? and name like '#%'",[args[1]])[0];
+									if(server!==undefined&&channel!==undefined){
+											var data = {
+													s_id: args[0],
+													server: server.name,
+													c_id: args[1],
+													channel: channel.name,
+													date: args[2],
+													pastDate: ts(pastDate),
+													todayDate: ts(d),
+													nextDate: ts(nextDate),
+													messages: []
+												},
+												m,t,
+												htmlent = function(text){
+													return String(text).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+												},
+												parse = function(m){
+													var style="",
+														c,
+														bg,
+														t=m[0];
+													switch(t){
+														case "\x0f":
+															t = "\x03";
+														break;
+														case "\x1f":
+															t = "\x03";
+															style="text-decoration:underline;";
+														break;
+														case "\x02":
+															t="\x03";
+															style="font-weight:bold;";
+														break;
+														case "\x03":
+															c=m[1];
+															if(/\d/.test(m[2])){
+																c+=m[2];
+																if(m[3] == ','){
+																	bg = m[4];
+																	if(/\d/.test(m[5])){
+																		bg+=m[5];
+																	}
+																}
+															}else if(m[2] == ','){
+																bg = m[3];
+																if(/\d/.test(m[4])){
+																	bg+=m[4];
+																}
+															}
+														break;
+													}
+													return '</span>'+chunk(c,bg,style);
+											},
+											colourNick = function(nick,id){
+												nick = htmlent(nick);
+												var hash = (function(){
+														var h = 0,
+															i;
+														for(i=0;i<nick.length;i++){
+															h = nick.charCodeAt(i)+(h<<6)+(h<<16)-h;
+														}
+														return h;
+													})(),
+													deg = hash%360,
+													hue = deg<0?360+deg:deg,
+													light = hue>=30&&hue<=210?30:50,
+													saturation = 20+Math.abs(hash)%80;
+												return templates.user.compile({
+													nick: nick,
+													hue: hue,
+													saturation: saturation,
+													light: light,
+													id: id
+												});
+											},
+											getColour = function(num,def){
+												var c = [
+													'white',
+													'black',
+													'blue',
+													'green',
+													'red',
+													'brown',
+													'purple',
+													'orange',
+													'yellow',
+													'lime',
+													'teal',
+													'aqua',
+													'royalblue',
+													'fuchsia',
+													'grey',
+													'silver'
+												][parseInt(num,0)];
+												return c===undefined?def:c;
+											},
+											chunk = function(c,bg,style){
+												style = style===undefined?'':style;
+												return "<span style='color:"+getColour(c,'black')+";background-color:"+getColour(bg,'transparent')+";"+style+"'>";
+											},
+											links = function(href){
+												return isdomain(href)?'<a href="'+toUrl(href)+'">'+href+'</a>':href;
+											},
+											ds = {},
+											id,
+											i;
+										for(i in r){
+											m = r[i];
+											t = htmlent(m.text)
+												.replace(/\b((?:\w*:?\/\/)?\w+\.\w\w+\/?[A-Za-z0-9_.-~\-]*#?[A-Za-z0-9_.\-~\-]*)\b/g,links)
+												.replace(/[\x02\x1f\x16\x0f]|\x03(\d{0,2}(?:,\d{0,2})?)/g,parse)
+												.trim();
+											id = m.time;
+											if(ds[id]!==undefined){
+												id = id+'-'+m.id;
+											}
+											ds[id] = true;
+											data.messages.push({
+												id: id,
+												type: m.type,
+												datetime: m.datetime,
+												time: m.time,
+												body: chunk()+(templates.types[m.type]?templates.types[m.type]:templates.types.message).compile({
+													user: colourNick(m.user,m.u_id),
+													text: t,
+													channel: channel.name,
+													server: server.name,
+												})+'</span>'
+											});
+										}
+										res.write(templates.log.compile(data));
+									}else{
+										res.statusCode = 404;
+										res.write(templates['404'].compile({
+											message: 'Channel does not exist'
+										}));
+									}
+									res.end();
 								});
 							}
-							res.write(templates.log.compile(data));
-						}else{
-							res.statusCode = 404;
-							res.write(templates['404'].compile({
-								message: 'Channel does not exist'
-							}));
-						}
-						res.end();
-					});
+					}
 				}
 			break;
 		}
