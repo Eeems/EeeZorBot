ade04d320e4487fda3e9d086c2f5f0ce6c197e55
Nathaniel van Diepen <eeems@omnimaga.org>
2015-11-17 17:50:33 -0700

    Stuffs
    Handle channel better for events (No more allowing self references lol).
    Add user.send()
    Permissions on the manager script.
    See bans from IRC. Have to have the +b flag.

--- mods/manager/scripts/server.js
+++ mods/manager/scripts/server.js
@@ -4,21 +4,46 @@ server.add('uptime',function(){
 	.add('exit',function(){
 		if(this.user && this.user.owner){
 			process.exit();
+		}else{
+			this.user.send('Not Permitted');
 		}
 	},'Makes the bot exit')
 	.add('quit',function(){
 		if(this.user && this.user.owner){
 			server.quit();
 			log.log('quitting');
+		}else{
+			this.user.send('Not Permitted');
 		}
 	},'Makes the bot quit from the current server')
 	.add('+ban',function(argv){
 		if(this.user && this.user.owner && this.user.owner.flags.indexOf('b')){
 			bans.add(argv);
+			this.channel.send('Ban added');
+		}else{
+			this.user.send('Not Permitted');
 		}
 	})
 	.add('-ban',function(argv){
 		if(this.user && this.user.owner && this.user.owner.flags.indexOf('b')){
 			bans.remove(argv);
+			this.channel.send('Ban removed');
+		}else{
+			this.user.send('Not Permitted');
+		}
+	})
+	.add('bans',function(argv){
+		var t = this;
+		if(t.user && t.user.owner && t.user.owner.flags.indexOf('b')){
+			if(bans.length){
+				t.user.send('Bans:');
+				bans.each(function(ban){
+					t.user.send(' - '+ban.hostmask);
+				});
+			}else{
+				t.user.send('No bans.');
+			}
+		}else{
+			t.user.send('Not Permitted');
 		}
 	});
